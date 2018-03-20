f6abf0f1a9c5a4a253062cb5afa916f8697f7465
Giuseppe Paolo <giuseppe.paolo93@gmail.com>
2018-03-20 10:04:07 +0000

    set_up_system.sh created online with Bitbucket

---
+++ set_up_system.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu -o pipefail # fail on error , debug all lines
+
+echo installing the must-have pre-requisites
+while read -r p ; do apt-get install -y $p ; done < <(cat << "EOF"
+    git
+    wget
+    zsh
+    nano
+    python3
+EOF
+)
+# Make zsh default
+chsh -s $(which zsh)
+
+# Create src folder to download stuff
+mkdir ./src
+cd src
+
+# Install pip and packages for virtualenv
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+pip install pipenv
+
+# Install oh-my-zsh and make it default
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
