b7f750e38690384a456113e90daa88534c678168
GPaolo93 <giuseppe.paolo@softbankrobotics.com>
2019-09-19 12:05:09 +0200

    Added install singularity scripts

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Utils
 
-This repo contains some setup files for new systems
+This repo contains some setup files for new systems.
+Just run `set_up_system.sh` as sudo and should install the zsh shell, tmux, git, python, pip etc
+
+In `install_singularity` there are the scripts to install the singularity container manager.
---
+++ install_singularity/zsh_install_singularity.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+apt-get update
+apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev squashfs-tools libseccomp-dev wget pkg-config git cryptsetup-bin
+
+# DOWNLOAD GO
+GO_VERSION=1.13
+OS=linux
+ARCH=amd64
+sudo -u $USER wget https://dl.google.com/go/go${GO_VERSION}.${OS}-${ARCH}.tar.gz
+tar -C /usr/local -xzf go${GO_VERSION}.${OS}-${ARCH}.tar.gz
+sudo -u $USER rm go${GO_VERSION}.${OS}-${ARCH}.tar.gz
+# SETUP GO ENVIRONMENT
+FILE=~/.zshrc
+sudo -u $USER echo 'export GOPATH=${HOME}/go' >> ${FILE}
+sudo -u $USER echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ${FILE}
+sudo -u $USER source ${FILE}
+
+# Download Singularity
+SING_VERSION=3.4.0
+sudo -u $USER wget https://github.com/sylabs/singularity/releases/download/v${SING_VERSION}/singularity-${SING_VERSION}.tar.gz
+sudo -u $USER tar -xzf singularity-${VERSION}.tar.gz
+sudo -u $USER singularity
+# Compile Singularity
+sudo -u $USER ./mconfig
+sudo -u $USER make -C ./builddir
+make -C ./builddir install
+
+# Test Singularity
+sudo -u $USER singularity --version
