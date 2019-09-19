9b7f4e04f29960a258271decfa63c387f3e57516
Giuseppe Paolo <giuseppe.paolo93@gmail.com>
2019-09-19 10:01:30 +0000

    remove unwanted sudos

--- set_up_system.sh
+++ set_up_system.sh
@@ -9,22 +9,22 @@ apt-get install -y git wget zsh nano python3 python3-pip python3-dev tmux htop
 chsh -s $(which zsh)
 
 # Create src folder to download stuff
-mkdir -p ./src
-cd src
+sudo -u $USER mkdir -p ./src
+sudo -u $USER cd src
 
 # Install pip and packages for virtualenv
-wget https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
-pip install pipenv
+sudo -u $USER wget https://bootstrap.pypa.io/get-pip.py
+sudo -u $USER python3 get-pip.py
+sudo -u $USER pip install pipenv
 
 # Install oh-my-zsh and make it default
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sudo -u $USER sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-echo "export PIPENV_VENV_IN_PROJECT=1" >> ~/.zshrc
-echo "export SHELL=/usr/bin/zsh" >> ~/.zshrc
-echo "export LC_ALL=C.UTF-8" >> ~/.zshrc
-echo "export LANG=C.UTF-8" >> ~/.zshrc
+sudo -u $USER echo "export PIPENV_VENV_IN_PROJECT=1" >> ~/.zshrc
+sudo -u $USER echo "export SHELL=/usr/bin/zsh" >> ~/.zshrc
+sudo -u $USER echo "export LC_ALL=C.UTF-8" >> ~/.zshrc
+sudo -u $USER echo "export LANG=C.UTF-8" >> ~/.zshrc
 
 # Create tmux conf
-tmux
-tmux show -g | sed 's/^/set -g /' > ~/.tmux.conf
+sudo -u $USER tmux
+sudo -u $USER tmux show -g | sed 's/^/set -g /' > ~/.tmux.conf
